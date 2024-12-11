7f07c5468a7af2b21794fbc32fc6f3d6c9b0391d
PQCraft <58464017+PQCraft@users.noreply.github.com>
2024-12-10 20:37:27 -0500

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -18,24 +18,24 @@ bot.message(starting_with: 'wii$') do |event|
       help += "`wii$uname` - Runs uname and sends the results\n"
       help += "`wii$say`- Send a message and display it on Jaeger\'s monitors\n"
       help += "`wii$draw`- Draw some graphics on Jaeger\'s monitors"
-      event.respond help
+      event.respond(help)
     elsif command == 'say'
       print event.user.name,": ", arg, "\n"
-      event.respond "Message sent!"
+      event.respond("Message sent!")
     elsif command == 'draw'
       arg = arg.gsub("'", "'\\''")
       fbdraw = `./fbdraw /dev/fb0 '#{arg}' 2>&1`
       fbdraw = fbdraw.gsub("`", "\u200b`")
-      event.respond "```ansi\n" + fbdraw + "\n```" 
+      event.respond((fbdraw.empty?) ? "Drawing sent!" : "```ansi\n" + fbdraw + "\n```")
     elsif command == 'uname'
       uname = `uname -a`
-      event.respond uname
+      event.respond(uname)
     elsif command == 'neofetch'
       neofetch = `neofetch`
       neofetch = neofetch.gsub(/\x1b(?:[@-Z\\-_]|\[(?!([0-9]|[2-4][0-9])?m)[0-?]*[ -\/]*[@-~])/, "")
       neofetch = neofetch.gsub("\x1b[m", "\x1b[0m")
       neofetch = neofetch.gsub("`", "\u200b`")
-      event.respond "```ansi\n" + neofetch + "\n```" 
+      event.respond("```ansi\n" + neofetch + "\n```")
     else
       event.respond("Unknown command: #{command}")
     end
